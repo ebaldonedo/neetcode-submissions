ed3af9f23cca14b804e62b14de97cb89d8e7a35d
Enmanuel <50787415+ebaldonedo@users.noreply.github.com>
2026-07-27 11:29:06 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const dict= new Map<number,number>
+        for(let i=0;i<nums.length;i++){
+            if(dict.has(nums[i])){
+                return true
+            }else{
+                dict.set(nums[i],1)
+            }
+
+        }
+       
+        return false
+    }
+}
