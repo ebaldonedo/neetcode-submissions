6de899232234a81133cdc881e0a0a895a5eb6c8a
Enmanuel <50787415+ebaldonedo@users.noreply.github.com>
2026-07-27 11:41:30 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length!=t.length) return false
+        const dict=new Map<string,number>
+    //1.-Counting letters
+        for(let i =0;i<s.length;i++){
+            if(dict.has(s[i])){
+                const q= dict.get(s[i])
+                dict.set(s[i],q+1)
+                continue
+            }
+            dict.set(s[i],1)  
+        }
+    //2.-Evaluating if are anagrams
+    for(let i=0;i<t.length;i++){
+        const q = dict.get(t[i])
+        if(!q) return false
+        dict.set(t[i],q-1)
+         
+        
+
+    }   
+
+        
+
+        return true
+    }
+}
