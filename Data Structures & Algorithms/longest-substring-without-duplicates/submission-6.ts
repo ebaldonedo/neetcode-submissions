d614098d75c010b2badffafaa68161d707b77a5a
Enmanuel <50787415+ebaldonedo@users.noreply.github.com>
2026-07-28 15:04:41 -0400

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        if(s.length === 0)return 0
+        const dict = new Map<string,number>()
+        let max = 1
+        for(let i = 0; i < s.length; i++){
+            dict.clear(); // <-- Esto soluciona el problema
+            const letter = s[i]
+            let cont = 1
+            dict.set(letter,1)
+            for(let n=i+1; n<s.length; n++){
+               if(dict.has(s[n])){
+                cont=1
+                dict.clear()
+                break
+               }else{
+                cont++
+                dict.set(s[n],1)
+               }
+                max = Math.max(max,cont)
+            }
+        }
+        return max
+    }
+}
