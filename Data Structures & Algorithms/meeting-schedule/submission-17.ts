4adc3560ffdb63b7c29d73dee5fc7367f0880c7e
Enmanuel <50787415+ebaldonedo@users.noreply.github.com>
2026-07-30 15:36:33 -0400

    Add: meeting-schedule - submission-17

---
+++ Data Structures & Algorithms/meeting-schedule/submission-17.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval  {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals: Interval[]): boolean {
+        intervals=intervals.sort((a,b)=>a.start-b.start)
+        for(let i =1;i<=intervals.length-1;i++){
+            console.log(intervals[i].start,intervals[i-1].end)
+            if(intervals[i].start<intervals[i-1].end){
+                return false
+            }
+
+
+        }
+        return true
+        
+    }
+}
